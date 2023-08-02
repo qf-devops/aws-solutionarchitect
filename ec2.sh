f8109ffabf20c5d0df05a36cca4fd9056ac6b059
Ravi <ravi@vytec.com>
2023-08-02 22:17:22 +0530

    updated

--- ec2.sh
+++ ec2.sh
@@ -1,1 +1,3 @@
 aws ec2 run-instances --image-id ami-0ded8326293d3201b --count 1 --instance-type t2.micro --key-name mumbai --security-group-ids sg-0fbebfd9572febce5 --subnet-id subnet-e58fafa9
+
+aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
